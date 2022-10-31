2f8b1110aea718326edcb9cd31c6c558d0236701
kamilsmr <kamils6035@gmail.com>
2022-10-31 23:41:24 +0300

    continue

---
+++ app.js
@@ -0,0 +1,7 @@
+let myBlock;
+        document.addEventListener("DOMContentLoaded", function () {
+            console.log("ready");
+            myBlock = document.createElement("div");
+            myBlock.textContent = "hello world";
+            document.body.appendChild(myBlock);
+        })
