23698de8180bfc7f6ef5b6d660847e9ec8a4cdb1
kamilsmr <kamils6035@gmail.com>
2022-10-31 23:42:24 +0300

    style

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 let myBlock;
         document.addEventListener("DOMContentLoaded", function () {
-            console.log("ready");
             myBlock = document.createElement("div");
             myBlock.textContent = "hello world";
+            myBlock.style.width = "100px";
+            myBlock.style.height = "100px";
+            myBlock.style.backgroundColor = "red";
+            myBlock.style.color = "white";
+            myBlock.style.lineHeight = "100px";
+            myBlock.style.textAlign = "center";
+            myBlock.style.position = "absolute";
+            myBlock.style.top = "100px";
+            myBlock.style.left = "150px";
             document.body.appendChild(myBlock);
         })
